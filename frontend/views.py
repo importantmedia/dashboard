32dfa665c714093372429760e4015d86e76b18d3
Nick Sullivan <nick@kruxdigital.com>
2012-03-22 18:37:07 -0700

    tag generator

--- frontend/views.py
+++ frontend/views.py
@@ -1,1 +1,10 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from frontend.models import Publisher, Tag
+
+def tag_generator(request):
+    context = {
+        "publishers": Publisher.objects.all(),
+        "sizes": dict(Tag.SIZE_CHOICES)
+    }
+    return render_to_response("tools/tag_generator.html", context, RequestContext(request))
--- urls.py
+++ urls.py
@@ -4,12 +4,13 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'liftium.views.home', name='home'),
-    # url(r'^liftium/', include('liftium.foo.urls')),
+    # url(r'^$', 'frontend.views.home', name='home'),
+    # url(r'^frontend/', include('frontend.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
     # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
+    url(r'tag_generator', 'frontend.views.tag_generator'),
     url(r'dashboard/', include(admin.site.urls)), # for the dev server
     url(r'', include(admin.site.urls)), # for apache
 )
