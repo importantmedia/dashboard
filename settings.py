26004c7ed5a94d8ca1ceeac65e56c4479931853e
Nick Sullivan <nick@sullivanflock.com>
2012-03-27 23:07:25 -0700

    /var/www/liftiumenv/lib/python2.6/site-packages/django/conf/__init__.py:75: DeprecationWarning: The ADMIN_MEDIA_PREFIX setting has been removed; use STATIC_URL instead.

--- settings.py
+++ settings.py
@@ -65,11 +65,6 @@ STATIC_ROOT = ROOT_PATH + '/collected_static/'
 # Example: "http://media.lawrence.com/static/"
 STATIC_URL = '/dashboard/static/'
 
-# URL prefix for admin static files -- CSS, JavaScript and images.
-# Make sure to use a trailing slash.
-# Examples: "http://foo.com/static/admin/", "/static/admin/".
-ADMIN_MEDIA_PREFIX = '/dashboard/static/admin/'
-
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
