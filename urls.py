f8975479b3cd46af3a69ec3ce7c51e08b28d19b7
Nick Sullivan <nick@sullivanflock.com>
2012-04-09 23:51:57 -0700

    fixed up urls to deal with rejigger of document roots

--- urls.py
+++ urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.views.generic.simple import redirect_to
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -7,10 +8,7 @@ urlpatterns = patterns('',
     # url(r'^$', 'frontend.views.home', name='home'),
     # url(r'^frontend/', include('frontend.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    url(r'tag_generator', 'frontend.views.tag_generator'),
-    url(r'dashboard/', include(admin.site.urls)), # for the dev server
-    url(r'', include(admin.site.urls)), # for apache
+    url(r'^tag_generator', 'frontend.views.tag_generator'),
+    url(r'^dashboard/', include(admin.site.urls)), 
+    url(r'^$',  redirect_to, {'url': '/dashboard/'})
 )
